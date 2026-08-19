2d14f53cfdc8db62d085492389807d0b4166d40e
Factorizando <factorizandoeluniverso@gmail.com>
2026-08-18 19:54:52 -0600

    Updates

--- assets/Preparatoria-DZ23Vg3K.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-_uuaRWKs.js";import{S as t}from"./SubjectPage-Mk40U6tF.js";import{S as a}from"./preparatoriaData-BWFIp7Ab.js";import"./BrandName-CPNuyZCh.js";function i(){return r.jsx(t,{level:"preparatoria",subjects:a})}export{i as default};
---
+++ assets/Universidad-D_B3LwC_.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-wXEv7i_3.js";import{S as a}from"./SubjectPage-Bhpms97O.js";import{S as e,a as i}from"./exaniIIData-CU_Wj_kp.js";import"./BrandName-D1R3yziQ.js";const t=[{id:"unam",label:"UNAM",subjects:e},{id:"exani-ii",label:"EXANI-II",subjects:i}];function u(){return s.jsx(a,{level:"universidad",tabs:t})}export{u as default};

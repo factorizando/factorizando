ace05716cb98d61e33ead37bbaea98b6f33a1949
Factorizando <factorizandoeluniverso@gmail.com>
2026-08-25 23:45:35 -0600

    Updates

--- assets/Universidad-DTXonp3Z.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Dv2Wn7T-.js";import{S as a}from"./SubjectPage-BDXlXo5b.js";import{S as e,a as i}from"./exaniIIData-CU_Wj_kp.js";import"./BrandName-BEkStnq3.js";const t=[{id:"unam",label:"UNAM",subjects:e},{id:"exani-ii",label:"EXANI-II",subjects:i}];function u(){return s.jsx(a,{level:"universidad",tabs:t})}export{u as default};

be881dedf63f2a43905b4c0c51676e7e8ff2d752
Factorizando <factorizandoeluniverso@gmail.com>
2026-09-21 14:06:23 -0600

    Updates

--- assets/Preparatoria-uWRUfXDa.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Bd36RRGt.js";import{S as t}from"./SubjectPage-1jC9SNS7.js";import{S as a}from"./preparatoriaData-BWFIp7Ab.js";import"./BrandName-D16UXFuq.js";function i(){return r.jsx(t,{level:"preparatoria",subjects:a})}export{i as default};

b7f7e94e39acba1b7c5fbfee2e55b4eb37b98ebd
Factorizando <factorizandoeluniverso@gmail.com>
2026-09-22 17:22:34 -0600

    Updates

--- assets/Universidad-3eIE2f9M.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-B-GolWzK.js";import{S as a}from"./SubjectPage-_8gddgNw.js";import{S as e,a as i}from"./exaniIIData-CU_Wj_kp.js";import"./BrandName-6YBHIQbe.js";const t=[{id:"unam",label:"UNAM",subjects:e},{id:"exani-ii",label:"EXANI-II",subjects:i}];function u(){return s.jsx(a,{level:"universidad",tabs:t})}export{u as default};
---
+++ assets/Universidad-DY0XlFQl.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BkpKbGH2.js";import{S as a}from"./SubjectPage-Bj_ditMT.js";import{S as e,a as i}from"./exaniIIData-CU_Wj_kp.js";import"./BrandName-C6HX7kWE.js";const t=[{id:"unam",label:"UNAM",subjects:e},{id:"exani-ii",label:"EXANI-II",subjects:i}];function u(){return s.jsx(a,{level:"universidad",tabs:t})}export{u as default};

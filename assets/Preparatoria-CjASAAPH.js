7a87967953b3d3de3673306640cf39e87688716a
Factorizando <factorizandoeluniverso@gmail.com>
2026-09-23 20:01:31 -0600

    Updates

--- assets/Preparatoria-CjASAAPH.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CRxS88FY.js";import{S as t}from"./SubjectPage-DQnZxfqp.js";import{S as a}from"./preparatoriaData-BWFIp7Ab.js";import"./BrandName-CxArzusH.js";function i(){return r.jsx(t,{level:"preparatoria",subjects:a})}export{i as default};
